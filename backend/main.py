a030a63745dda3416c4d91bc64360d2e8ab07d56
kocsiatt <kocsiatt@gmail.com>
2026-04-09 00:31:00 +0200

    foglalas torles

--- backend/main.py
+++ backend/main.py
@@ -25,32 +25,39 @@ def get_restaurant(restaurant_id):
 
 @app.route("/restaurants/<string:restaurant_id>/bookings", methods=["GET"])
 def list_all_bookings(restaurant_id):
-    return jsonify(mongo.db.restaurants.find_one({"_id" : ObjectId(restaurant_id)}, {"bookings": 1}))
+    return jsonify(mongo.db.bookings.find({"restaurant_id" : ObjectId(restaurant_id)}))
 
 @app.route("/restaurants/<string:restaurant_id>/bookings/<string:date>", methods=["GET"])
 def list_bookings(restaurant_id, date):
-    return jsonify(mongo.db.restaurants.find_one({"_id" : ObjectId(restaurant_id), "bookings.date": date}, {"bookings.$": 1}))
+    return jsonify(mongo.db.bookings.find_one({"restaurant_id" : ObjectId(restaurant_id), "date": date}))
 
 @app.route("/restaurants/<string:restaurant_id>/bookings/<string:date>/booked-hours/<int:time>/available", methods=["GET"])
 def available_tables(restaurant_id, date, time):
-    restaurant = mongo.db.restaurants.find_one({"_id": ObjectId(restaurant_id), "bookings.date": date},
-                                  {"open_at": 1, "close_at": 1, "table_size_counts": 1, "bookings.$": 1})
+    booking = mongo.db.bookings.find_one({"date": date, "restaurant_id": ObjectId(restaurant_id)})
+    restaurant = mongo.db.restaurants.find_one({"_id": ObjectId(restaurant_id)},
+                                               {"open_at": 1, "close_at": 1, "table_size_counts": 1})
+
+    if booking is None:
+        return jsonify(restaurant["table_size_counts"])
+
     table_size_counts = restaurant["table_size_counts"]
-    if str(time) in restaurant["bookings"][0]["booked_hours"]:
-        booked_tables = restaurant["bookings"][0]["booked_hours"][str(time)]
+    if str(time) in booking["booked_hours"]:
+        booked_tables = booking["booked_hours"][str(time)]
         for booked_table in booked_tables:
             table_size_counts[str(booked_table["table_size"])] = table_size_counts[str(booked_table["table_size"])] - 1
 
     return jsonify(table_size_counts)
 
-@app.route("/restaurants/<string:restaurant_id>/bookings/<string:date>/booked_hours/<int:time>", methods=["DELETE"])
+@app.route("/restaurants/<string:restaurant_id>/bookings/<string:date>/booked-hours/<int:time>", methods=["DELETE"])
 def delete_booking(restaurant_id, date, time):
-    # TODO implement delete
+    customer_name = request.args.get('customer-name')
+    mongo.db.bookings.update_one({"restaurant_id": ObjectId(restaurant_id), "date": date}, { "$pull": {"booked_hours.{}".format(time): {"customer_name" : customer_name}}} )
     return '', 204
 
 @app.route("/restaurants/<string:restaurant_id>/bookings", methods=["POST"])
 def create_booking(restaurant_id):
     restaurant = mongo.db.restaurants.find_one({"_id" : ObjectId(restaurant_id)}, {"name": 1, "open_at": 1, "close_at": 1, "table_size_counts": 1})
+    print(restaurant)
     if restaurant is None:
         return {"error": "Invalid offset or limit"}, 404
 
@@ -65,8 +72,7 @@ def create_booking(restaurant_id):
     if str(requested_booking["table_size"]) not in restaurant["table_size_counts"]:
         return jsonify({"error": "We do not have an appropriate table. Available table sizes: {}".format(", ".join(map(lambda s: s, restaurant["table_size_counts"])))}), 400
 
-    bookings = mongo.db.restaurants.find_one({"_id" : ObjectId(restaurant_id), "bookings.date": requested_booking["date"]}, {"bookings.$": 1})
-    day = (bookings and bookings["bookings"][0]) or { "date" : requested_booking["date"], "booked_hours" : {} }
+    day = mongo.db.bookings.find_one({"restaurant_id" : ObjectId(restaurant_id), "date": requested_booking["date"]})  or { "date" : requested_booking["date"], "booked_hours" : {} }
 
     if str(requested_booking["time"]) not in day["booked_hours"]:
         day["booked_hours"][str(requested_booking["time"])] = []
@@ -78,11 +84,7 @@ def create_booking(restaurant_id):
         return jsonify({"error": "All table of size {} are booked.".format(requested_booking["table_size"])}), 400
     booked_hour.append({"customer_name" : requested_booking["customer_name"], "table_size" : requested_booking["table_size"]})
 
-    if bookings is None:
-        mongo.db.restaurants.update_one({"_id": ObjectId(restaurant_id)}, {"$push": {"bookings": day}})
-    else:
-        mongo.db.restaurants.update_one({"_id": ObjectId(restaurant_id), "bookings.date": requested_booking["date"]},
-                                        {"$set": {"bookings.$": day}})
+    mongo.db.bookings.replace_one({"restaurant_id" : ObjectId(restaurant_id), "date": requested_booking["date"]}, upsert=True, replacement=day)
 
     return '', 204
 
